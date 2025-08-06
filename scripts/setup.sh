fa657ce9411e98c15eee70a4226dd6089ffc4955
Subin Song <subin@subinsong.com>
2025-08-06 20:49:06 +0900

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,7 +16,6 @@ sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 sudo systemctl enable docker
