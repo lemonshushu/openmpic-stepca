a0521bb5a0f7a54330377b1ec3aac43bea20ab06
Subin Song <subin@subinsong.com>
2025-08-09 04:17:31 +0000

    finalize

--- scripts/install_step.sh
+++ scripts/install_step.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# install step
+# install step cli
 apt-get update && apt-get install -y --no-install-recommends curl vim gpg ca-certificates
 curl -fsSL https://packages.smallstep.com/keys/apt/repo-signing-key.gpg -o /etc/apt/trusted.gpg.d/smallstep.asc && \
     echo 'deb [signed-by=/etc/apt/trusted.gpg.d/smallstep.asc] https://packages.smallstep.com/stable/debian debs main' \
